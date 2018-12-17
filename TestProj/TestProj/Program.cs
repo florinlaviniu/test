598907ee754f80c955bb8ec717b5acefc2912cd0
florin <florinlaviniu@yahoo.com>
2018-12-17 19:49:27 +0200

    added test

--- TestProj/TestProj/Program.cs
+++ TestProj/TestProj/Program.cs
@@ -8,14 +8,68 @@ namespace TestProj
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            
-            Console.WriteLine(Convert.ToInt32(args[0].ToString()) + Convert.ToInt32(args[1].ToString()));
-            Console.ReadKey();
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.Write("Triangle Type Test :-\n----------------------------\n\n");
+            Trigonometric.TrianlgeSidesInput(12, 12, 12);
+            Console.ReadLine();
+
+            Trigonometric.TrianlgeSidesInput(12, 12, 15);
+            Console.ReadLine();
+
+            Trigonometric.TrianlgeSidesInput(12, 20, 25);
+            Console.ReadLine();
+        }
+
+       
+    }
+
+    public class Trigonometric
+    {
+        public static double eqSide1, eqSide2, eqSide3;
+
+        public static void TrianlgeSidesInput(double side1, double side2, double side3)
+        {
+            string TriangleType = "";
+            if ((side1 == side2) && (side1 == side3))
+            {
+                TriangleType = "Equilateral";
+            }
+            else if ((side1 == side2) || (side1 == side3) || (side2 == side3))
+            {
+                TriangleType = "Isosceles";
+                eqSide1 = side1; eqSide2 = side2; eqSide3 = side3;
+            }
+            else
+            {
+                TriangleType = "Scalene";
+            }
+
+            switch (TriangleType)
+            {
+                case "Equilateral":
+                    Console.WriteLine("It is Equilateral Traingle ! Reason: It's all sides are equal.\n\n\t i. e. {0}=={1}==                                 {2}", side1, side2, side3);
+                    Console.WriteLine("For Next : Press Enter \n");
+
+                    break;
+
+                case "Isosceles":
+                    Console.WriteLine("It is Isosceles Traingle ! Reason: It's two sides are equal.");
+                    Console.WriteLine("For Next : Press Enter \n");
+                    break;
+
+                case "Scalene":
+                    Console.WriteLine("It is Scalene Traingle ! Reason: It's all sides have different lenght !!");
+                    Console.WriteLine("For Next : Press Enter \n");
+                    break;
+
+                default:
+                    Console.WriteLine("Sorry ! Try Again !");
+                    break;
+            }
+
         }
     }
 }
